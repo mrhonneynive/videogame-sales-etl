fea8f486c43634020d43df061aece6866571991c
mrhonneynive <38505964+mrhonneynive@users.noreply.github.com>
2025-02-21 01:00:31 +0300

    Configure PostgreSQL connection pool

---
+++ etl/etl.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+const csv = require("csv-parser");
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+});
+
+const csvFilePath = process.env.CSV_PATH;
