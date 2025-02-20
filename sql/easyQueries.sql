128109e2330a975c3e17714e4217ac1b367261ce
mrhonneynive <38505964+mrhonneynive@users.noreply.github.com>
2025-02-21 02:17:33 +0300

    Add a query that fetches top 10 best selling games

--- sql/easyQueries.sql
+++ sql/easyQueries.sql
@@ -1,2 +1,11 @@
 SELECT * FROM games
 LIMIT 10;
+
+-- top 10 best selling games
+SELECT games.name, SUM(sales.units_sold) AS total_sales
+FROM games, sales
+WHERE games.game_id = sales.game_id
+AND sales.region = 'Global'
+GROUP BY games.name
+ORDER BY total_sales DESC
+LIMIT 10;
